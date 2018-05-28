dcf7c60384a01ca1fd2e35f57ec1d7fe1f938fde
GhaithHan <ghaith.dev@gmail.com>
2018-05-28 18:06:21 +0100

    Done

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,5 +1,12 @@
-// Names and Input
+// Namvar hacker1 = "Marie";
 
+console.log("The dirver's name is " + hacker1);
+
+
+
+var hacker2 = "Ghaith";
+
+console.log("The navigator's name is " + hacker2);
 
 //Conditionals
 
